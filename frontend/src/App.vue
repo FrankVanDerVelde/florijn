ce75278be546981e3b1bb5e808c36ab1b692132a
Frank <frankvdv1998@gmail.com>
2022-10-12 00:10:53 +0200

    Added fonts, made a new version of the input and started work on profile page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <NavBar></NavBar>
-    <div class="p-8 z-10">
+    <!-- <div class="p-8 z-10">
       <h1 class="text-xl font-bold mb-4">Components</h1>
 
       <div class="flex flex-col gap-8">
@@ -20,7 +20,7 @@
           <TextField :label="'test'" class="m-8"></TextField>
         </div>
       </div>
-    </div>
+    </div> -->
   </div>
   <router-view></router-view>
 </template>
@@ -30,3 +30,7 @@
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import HelloWorld from './components/HelloWorld.vue'
 </script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
+</style>
--- frontend/src/components/Profile.vue
+++ frontend/src/components/Profile.vue
@@ -1,9 +1,78 @@
-<template>
-   <div>
-    Profile page
-   </div>
-  </template>
-  
-  <style scoped>
 
-  </style>
+
+<template>
+    <div class="flex justify-center">
+    <div class="profile-container flex self-center w-4/5">
+        <div class="side-bar w-1/6">
+            <span>Profiel</span>
+            <span>CV</span>
+        </div>
+        <!-- <FormInput :name="test">test</FormInput> -->
+        <div class="form grow">
+
+            <div class="input-container duo">
+                <FormInput name="voornaam"></FormInput>
+                <FormInput name="achternaam"></FormInput>
+            </div>
+
+            <span class="adress">Woongegevens</span>
+            <div class="input-container">
+                <FormInput name="Email" type="email"></FormInput>
+            </div>
+
+            <div class="input-container duo">
+                <FormInput name="woonplaats"></FormInput>
+                <FormInput name="postcode"></FormInput>
+            </div>
+
+            <div class="input-container duo">
+                <FormInput name="huisnummer"></FormInput>
+                <FormInput name="toevoeging"></FormInput>
+            </div>
+
+            <span class="available-hours">Beschikbare uren</span>
+
+        </div>
+    </div>
+</div>
+</template>
+  
+<style scoped>
+.profile-container {}
+
+.input-container {
+    display: flex;
+    margin-bottom: 1em;
+}
+
+.input-container.duo {
+
+}
+
+.input-container.duo>div:first-child {
+    margin-right: 1em;
+}
+
+.adress {
+    font-family: 'Roboto', sans-serif;
+    font-weight: 600;
+    font-size: 20px;
+}
+
+.available-hours {
+    font-family: 'Roboto', sans-serif;
+    font-weight: 400;
+    font-size: 30px;
+}
+</style>
+
+<script>
+export default {
+    name: " Profile",
+    components: {
+        FormInput
+    }
+}
+import FormInput from "./Common/FormInput.vue";
+
+</script>
