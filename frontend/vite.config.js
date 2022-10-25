53c22e45ec40f934ba46623fc0b3fa33ca253eaa
FrankVanDerVelde <frankvdv1998@gmail.com>
2022-10-25 13:51:47 +0200

    Fix for postcss and vite warnigns

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue(),
+    nodePolyfills({
+      // Whether to polyfill `node:` protocol imports.
+      protocolImports: true,
+    }),]
 })
