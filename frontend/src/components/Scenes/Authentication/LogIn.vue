fce118edd650937d2f6ad8096ba4d5972d78bce7
GCNT <lukasvdgaag@gmail.com>
2022-10-26 01:17:29 +0200

    Reverted login to old design, added project overview margin bottom.

--- frontend/src/components/Scenes/Authentication/LogIn.vue
+++ frontend/src/components/Scenes/Authentication/LogIn.vue
@@ -1,4 +1,168 @@
 <template>
+  <div class="page-main-mw p-4">
+    <div class="container">
+      <div class="login-form">
+        <div class="border-bottom">
+          <p class="header-text">Log in</p>
+        </div>
+        <div class="inputfield-mail">
+          <div class="input-container">
+            <label class="mb-[12px]">Emailadres</label>
+            <input v-model="email" class="pl-[7px]">
+          </div>
+        </div>
+        <div class="inputfield-wachtwoord">
+          <div class="input-container">
+            <label class="mb-[12px]">Wachtwoord</label>
+            <input v-model="password" type="password" class="pl-[7px]">
+            <p class="wrongLogin">{{ validationText }}</p>
+          </div>
+        </div>
+        <div class="submit-button">
+          <button class="bg-primary-500 text-neutral-50 font-semibold hover:bg-primary-700 px-5 py-2.5
+          text-sm leading-5 rounded-lg w-fit" @click="submitButton()">Log in
+          </button>
+        </div>
+        <div class="forgot-password" @click="forgotPassword()">Wachtwoord vergeten?</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "LogIn.vue",
+  methods: {
+    forgotPassword() {
+      this.$router.push(this.$route.matched[0].path + "/forgotpassword");
+    },
+    submitButton() {
+      if (this.email === '' || this.password === '') {
+        this.validationText = 'De velden zijn niet volledig ingevuld!';
+        return;
+      }
+      for (let i = 0; i < this.users.length; i++) {
+        if (this.email === this.users[i].emailadress && this.password === this.users[i].password) {
+          console.log("Ingelogd met het id: " + this.users[i].id);
+          this.$router.push("/project-overview");
+          return;
+        }
+      }
+      this.validationText = 'De inloggegevens zijn onjuist ingevuld! Probeer het' +
+          ' nogmaals met een ander emailadres/wachtwoord.';
+      this.password = '';
+    }
+  },
+  data() {
+    return {
+      email: '',
+      password: '',
+      validationText: '',
+      users: [
+        {
+          id: 0,
+          emailadress: "jveerman@outlook.com",
+          password: "GebakkenEieren10",
+        }, {
+          id: 1,
+          emailadress: "rtol@outlook.com",
+          password: "Vuurtoren10",
+        }, {
+          id: 2,
+          emailadress: "test@test.com",
+          password: "test"
+        }
+      ],
+    };
+  },
+}
+</script>
+
+<style scoped>
+
+.container {
+  display: flex;
+  margin-top: 100px;
+  justify-content: center;
+  align-items: center;
+}
+
+.login-form {
+  border-radius: 6px;
+  width: 300px;
+  border: 2px solid #BFBFBF;
+  box-shadow: 0 7px 8px 2px rgba(0, 0, 0, 0.12);
+}
+
+.header-text {
+  font-weight: bold;
+  font-size: 20px;
+  margin: 3%;
+  min-height: 50px;
+}
+
+.border-bottom {
+  border-bottom: 1px solid var(--neutral-100);
+}
+
+.inputfield-mail {
+  margin: 3% 3% 9%;
+}
+
+.inputfield-wachtwoord {
+  margin: 3% 3% 9%;
+}
+
+.submit-button {
+  justify-content: center;
+  align-items: center;
+  margin: 3%;
+  text-align: center;
+}
+
+.forgot-password {
+  margin: 3% 3% 3%;
+  text-align: center;
+  color: #F35627;
+  font-weight: bold;
+  font-size: 17px;
+}
+
+.forgot-password:hover {
+  color: #C52707;
+}
+
+.wrongLogin {
+  color: red;
+}
+
+.input-container {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+}
+
+.input-container label {
+  font-family: 'Roboto', sans-serif;
+  font-weight: 600;
+  color: #7B8794;
+  font-size: 14px
+}
+
+.input-container input {
+  width: 100%;
+  color: #BFBFBF;
+  border: 1px solid #BFBFBF;
+  border-radius: 6px;
+  font-family: 'Roboto', sans-serif;
+  height: 40px;
+  font-size: 16px;
+}
+
+</style>
+<!--
+<template>
   <div class="container">
     <div class="page-main-mw p-4 container-form">
       <div class="change-password-form">
@@ -137,13 +301,13 @@ img {
   margin: 1%;
   float: right;
   font-weight: bold;
-  color: var(--primary-500);
+  color: var(&#45;&#45;primary-500);
   font-size: 14px;
   order: 2;
 }
 
 .forgot-password:hover {
-  color: var(--primary-800);
+  color: var(&#45;&#45;primary-800);
 }
 
 .wrongLogin {
@@ -160,13 +324,13 @@ img {
 .input-container label {
   font-family: 'Roboto', sans-serif;
   font-weight: 600;
-  color: var(--neutral-400);
+  color: var(&#45;&#45;neutral-400);
   font-size: 14px
 }
 
 .input-container input {
-  color: var(--neutral-200);
-  border: 1px solid var(--neutral-200);
+  color: var(&#45;&#45;neutral-200);
+  border: 1px solid var(&#45;&#45;neutral-200);
   border-radius: 6px;
   font-family: 'Roboto', sans-serif;
   height: 40px;
@@ -177,7 +341,7 @@ img {
   margin: 1%;
   float: left;
   font-size: 14px;
-  color: var(--neutral-50);
+  color: var(&#45;&#45;neutral-50);
   order: 1;
   display: block;
   position: relative;
@@ -199,19 +363,19 @@ img {
   left: 0;
   height: 25px;
   width: 25px;
-  background-color: var(--neutral-50);
-  border: 1px solid var(--neutral-300);;
+  background-color: var(&#45;&#45;neutral-50);
+  border: 1px solid var(&#45;&#45;neutral-300);;
   border-radius: 25%;
 }
 
 /* On mouse-over, add a grey background color */
 .radio-button-container:hover input ~ .checkmark {
-  background-color: var(--primary-300);
+  background-color: var(&#45;&#45;primary-300);
 }
 
 /* When the checkbox is checked, add a background */
 .radio-button-container input:checked ~ .checkmark {
-  background-color: var(--primary-500);
+  background-color: var(&#45;&#45;primary-500);
 }
 
 /* Create the checkmark/indicator (hidden when not checked) */
@@ -232,7 +396,7 @@ img {
   top: 5px;
   width: 5px;
   height: 10px;
-  border: solid var(--neutral-50);
+  border: solid var(&#45;&#45;neutral-50);
   border-width: 0 3px 3px 0;
   -webkit-transform: rotate(45deg);
   -ms-transform: rotate(45deg);
@@ -240,3 +404,4 @@ img {
 }
 
 </style>
+-->
