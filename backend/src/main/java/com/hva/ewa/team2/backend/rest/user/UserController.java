7d022aeba44899707c628174bb36dc14b0358a71
Thomas Tol <thomas.tol@hva.nl>
2022-11-22 22:41:35 +0100

    updated with feedback Lukas (@autowired on constructor, new name repository

--- backend/src/main/java/com/hva/ewa/team2/backend/rest/user/UserController.java
+++ backend/src/main/java/com/hva/ewa/team2/backend/rest/user/UserController.java
@@ -1,13 +1,9 @@
 package com.hva.ewa.team2.backend.rest.user;
 
 import com.hva.ewa.team2.backend.data.user.UserRepository;
-import com.hva.ewa.team2.backend.domain.models.user.Admin;
-import com.hva.ewa.team2.backend.domain.models.user.Specialist;
-import com.hva.ewa.team2.backend.domain.models.user.Client;
 import com.hva.ewa.team2.backend.domain.models.user.User;
 import com.hva.ewa.team2.backend.rest.user.json.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,25 +13,30 @@ import java.util.List;
 @ResponseBody
 public class UserController {
 
+
+    private UserRepository userRepo;
+
     @Autowired
-    private UserRepository repository;
+    public UserController(UserRepository userRepository) {
+        this.userRepo = userRepository;
+    }
 
     @GetMapping(path = "/users")
     public List<User> getAllUsers(){
-        return this.repository.getAllUsers();
+        return this.userRepo.getAllUsers();
     }
 
     @GetMapping(path = "/users/{role}")
     public List<User> getUsersByRole(@PathVariable String role){
-        return this.repository.getUsersByRole(role);
+        return this.userRepo.getUsersByRole(role);
     }
 
     @PostMapping(path = "/login")
     public ResponseEntity<JsonUserInfo> findUserByCredentials(@RequestBody JsonCredentials credentials) {
-        User user = this.repository.findUserByCredentials(credentials.getEmail(), credentials.getPassword());
+        User user = this.userRepo.findUserByCredentials(credentials.getEmail(), credentials.getPassword());
 
         if (user != null) {
-            String role = this.repository.findRoleByUser(user);
+            String role = this.userRepo.findRoleByUser(user);
             return ResponseEntity.ok().body(new JsonUserInfo(user.getId(), role));
         }
         return null;
@@ -43,29 +44,29 @@ public class UserController {
 
     @GetMapping("/user/{id}")
     public User findById(@PathVariable int id){
-        return this.repository.findById(id);
+        return this.userRepo.findById(id);
     }
 
     @PostMapping("/users/save/admin")
     public ResponseEntity<User> saveAdmin(@RequestBody JsonAdminInfo body){
-        User user = this.repository.saveAdmin(body);
+        User user = this.userRepo.saveAdmin(body);
         return ResponseEntity.ok().body(user);
     }
 
     @PostMapping("/users/save/specialist")
     public ResponseEntity<User> saveSpecialist(@RequestBody JsonSpecialistInfo body){
-        User user = this.repository.saveSpecialist(body);
+        User user = this.userRepo.saveSpecialist(body);
         return ResponseEntity.ok().body(user);
     }
 
     @PostMapping("/users/save/client")
     public ResponseEntity<User> saveClient(@RequestBody JsonClientInfo body){
-        User user = this.repository.saveClient(body);
+        User user = this.userRepo.saveClient(body);
         return ResponseEntity.ok().body(user);
     }
 
     @DeleteMapping("/user/{id}")
     public User deleteById(@PathVariable int id){
-        return this.repository.deleteById(id);
+        return this.userRepo.deleteById(id);
     }
 }
