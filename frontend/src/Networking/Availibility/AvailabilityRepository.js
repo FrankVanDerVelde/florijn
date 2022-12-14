50dfc5fa665bd3c5b0d71cf5c720eb04d1618b87
walm9 <martijn.van.der.wal@hva.nl>
2022-12-14 22:58:23 +0100

    Created NetworkClient and using it in AvailabilityRepository

--- frontend/src/Networking/Availibility/AvailabilityRepository.js
+++ frontend/src/Networking/Availibility/AvailabilityRepository.js
@@ -1,13 +1,14 @@
 import FetchService from "../../Services/FetchService.js";
 import {Availability} from "../../components/models/Availability.js";
-import {HttpMethods} from "../HttpMethods.js";
+import {HttpMethod} from "../HttpMethod.js";
+import {NetworkClient} from "../NetworkClient.js";
 
 export class AvailabilityRepository {
 
-    fetcher;
+    #networkClient;
 
     constructor() {
-        this.fetcher = new FetchService('');
+        this.#networkClient = new NetworkClient();
     }
 
     /**
@@ -17,15 +18,9 @@ export class AvailabilityRepository {
      * @return {Promise<[Availability]>}
      */
     async fetchAvailabilityForUserInWeek(userId, weekNumber) {
-        try {
-            const path = `/users/${userId}/availability?weekNumber=${weekNumber}`;
-            console.log(`fetchAvailabilityForUserInWeek - GET ${path}`);
-            let results = await this.fetcher.fetchJson(path);
-            return results.map(Availability.fromJSON);
-        } catch (e) {
-            console.error(e);
-            return e;
-        }
+        const path = `/users/${userId}/availability?weekNumber=${weekNumber}`;
+        let results = await this.#networkClient.executeRequest(path, HttpMethod.GET);
+        return results.map(Availability.fromJSON);
     }
 
     /**
@@ -34,27 +29,17 @@ export class AvailabilityRepository {
      * @param date
      * @param fromTime
      * @param toTime
-     * @return {Promise<*|null>}
+     * @return {Promise<Availability|null>}
      */
     async createAvailability(userId, date, fromTime, toTime) {
-        try {
-            const path = `/users/${userId}/availability`;
-            const body = {
-                date: date,
-                from: fromTime,
-                to: toTime
-            };
-
-            console.log(`createAvailability - POST ${path} with body:`);
-            console.log(body);
-
-            let result = await this.fetcher.fetchJsonMethod(path, HttpMethods.POST, body);
-            console.log(result);
-            return Availability.fromJSON(result);
-        } catch (e) {
-            console.error(e);
-            throw e;
-        }
+        const path = `/users/${userId}/availability`;
+        const body = {
+            date: date,
+            from: fromTime,
+            to: toTime
+        };
+        let result = await this.#networkClient.executeRequest(path, HttpMethod.POST, body);
+        return Availability.fromJSON(result);
     }
 
     /**
@@ -66,23 +51,14 @@ export class AvailabilityRepository {
      * @return {Promise<*|null>}
      */
     async updateAvailability(id, date, fromTime, toTime) {
-        try {
-            const path = `/availability/${id}/update`;
-            let body = {
-                date: date,
-                from: fromTime,
-                to: toTime
-            };
-
-            console.log(`updateAvailability - PUT ${path} with body:`);
-            console.log(body);
-
-            let result = await this.fetcher.fetchJsonMethod(path, HttpMethods.PUT, body);
-            return Availability.fromJSON(result);
-        } catch (e) {
-            console.error(e);
-            throw e;
-        }
+        const path = `/availability/${id}/update`;
+        let body = {
+            date: date,
+            from: fromTime,
+            to: toTime
+        };
+        let result = await this.#networkClient.executeRequest(path, HttpMethod.PUT, body);
+        return Availability.fromJSON(result);
     }
 
     /**
@@ -91,42 +67,25 @@ export class AvailabilityRepository {
      * @return {Promise<Availability>}
      */
     async deleteAvailability(id) {
-        try {
-            const path = `/availability/${id}/delete`;
-            console.log(`deleteAvailability - DELETE ${path}`);
-            let result = await this.fetcher.fetchJsonMethod(path, HttpMethods.DELETE);
-            return Availability.fromJSON(result);
-        } catch (e) {
-            console.error(e);
-            throw e;
-        }
+        const path = `/availability/${id}/delete`;
+        let result = await this.#networkClient.executeRequest(path, HttpMethod.DELETE);
+        return Availability.fromJSON(result);
     }
 
     /**
      * Gets the availability
      * @param id
-     * @return {Promise<*|null>}
+     * @return {Promise<Availability|null>}
      */
     async getAvailability(id) {
-        try {
-            const path = `/availability/${id}`;
-            console.log(`getAvailability - GET ${path}`);
-            let result = await this.fetcher.fetchJsonMethod(`/availability/${id}`, HttpMethods.GET);
-            return Availability.fromJSON(result);
-        } catch (e) {
-            console.error(e);
-            throw e;
-        }
+        const path = `/availability/${id}`;
+        let result = await this.#networkClient.executeRequest(path);
+        return Availability.fromJSON(result);
     }
 
     async copyToWeek(userId, weekNumber) {
-        try {
-            const path = `/users/${userId}/availability/weeks/${weekNumber}/set-on-next-week`;
-            let result = await this.fetcher.fetchJsonMethod(path, HttpMethods.POST);
-            return result.map(Availability.fromJSON);
-        } catch (e) {
-            console.log(e);
-            throw e;
-        }
+        const path = `/users/${userId}/availability/weeks/${weekNumber}/set-on-next-week`;
+        let result = await this.#networkClient.executeRequest(path, HttpMethod.POST);
+        return result.map(Availability.fromJSON);
     }
 }
