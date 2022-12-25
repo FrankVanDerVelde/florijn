bdd9354c31c0da69c579610c7ad18955102b53af
Frank <frankvdv1998@gmail.com>
2022-12-25 22:56:03 +0100

    Fixed resume styling and fixed firefbox bug

--- frontend/src/components/Scenes/Profile/Resume.vue
+++ frontend/src/components/Scenes/Profile/Resume.vue
@@ -11,20 +11,20 @@
             <li>Werk plekken</li>
             <li>Skills</li>
         </ol>
-
+   
         <div>
             <div class="">
                 <label for="resume-upload-input" class="cursor-pointer inline-block">
                     <div
-                        class="mw-[186px] inline-block h-[44px] border-neutral-100 border-[2px] rounded-md flex justify-center items-center">
+                        class="mw-[186px] h-[44px] border-neutral-100 border-[2px] rounded-md flex justify-center items-center mb-5">
 
                         <font-awesome-icon icon="fa-file" class="text-neutral-700 text-[20px] ml-[10px] mr-[10px]" />
                         <div class="text-[16px] text-neutral-700 mt-[2px]">{{ (this.resumeFile ? this.resumeFile.name :
                                 'Upload hier')
                         }}</div>
-                        <div class="bg-primary-100 w-[20px] h-[20px] rounded-lg ml-[10px] relative mt-[2px] mr-[15px]">
+                        <div class="bg-primary-100 w-[20px] h-[20px] rounded-full ml-[10px] relative mt-[2px] mr-[15px]">
                             <font-awesome-icon icon="fa-arrow-down"
-                                class="text-primary-500 text-[14px] absolute top-[3px] left-[4px]" />
+                                class="text-primary-500 text-[14px] absolute top-[3px] left-[5px]" />
 
                         </div>
                     </div>
@@ -32,37 +32,29 @@
                 <input @change="e => updateResume(e)" type="file" name="resume-upload-input" class="hidden"
                     id="resume-upload-input" accept=".pdf">
                 <div class="flex">
-                    <div class="bg-primary-500 text-neutral-0 active:bg-white:text-primary-500 flex justify-center items-center rounded-md w-[120px] h-[38px] mb-[10px] hover:bg-primary-600 capitalize font-bold text-[14px] text-center"
+                    <div class="bg-primary-500 text-neutral-0 active:bg-white:text-primary-500 flex justify-center items-center rounded-md w-[120px] h-[38px] mb-[10px] hover:bg-primary-600 capitalize font-bold text-[14px] text-center mr-5"
                         @click="handleProfileUpdate">
                         <div>CV bijwerken</div>
                     </div>
-                    <div class="bg-primary-500 text-neutral-0 active:bg-white:text-primary-500 flex justify-center items-center rounded-md w-[120px] h-[38px] mb-[10px] hover:bg-primary-600 capitalize font-bold text-[14px] text-center"
+
+                    <div v-if="this.user?.resumeURL" class="bg-primary-500 text-neutral-0 active:bg-white:text-primary-500 flex justify-center items-center rounded-md w-[120px] h-[38px] mb-[10px] hover:bg-primary-600 capitalize font-bold text-[14px] text-center"
                         @click="toggleModal">
                         <div>View resume</div>
                     </div>
                 </div>
 
+       
                 <div v-if="modalActive" class="bg-white absolute top-0 bottom-0 left-0 right-0 z-10 ">
-                    <!-- <div className="pdf-container relative">
-                    <div v-if="this.user.resumeURL"> -->
-                        <object :data="pdfSrc" class="w-full h-full pt-[60px]" type="application/pdf">
-                            <div>No online PDF viewer installed</div>
-                        </object>
-                    
-                        <!-- <vue-pdf-embed :source="pdfSrc" /> -->
-                    <!-- </div> -->
-                    <!-- <div v-else="this.resumeFile">
-                        <vue-pdf-embed :source="this.resumeFile" />
-                    </div>
-                </div> -->
+                   
+                    <object v-if="pdfSrc" :data="pdfSrc" class="w-full h-full pt-[60px]" type="application/pdf">
+                
+                    </object>
 
-                <div class="bg-primary-500 text-neutral-0 active:bg-white:text-primary-500 flex justify-center items-center rounded-md w-[120px] h-[38px] mb-[10px] hover:bg-primary-600 capitalize font-bold text-[14px] text-center fixed bottom-0 left-1/2 -translate-x-1/2"
+                    <div class="bg-primary-500 text-neutral-0 active:bg-white:text-primary-500 flex justify-center items-center rounded-md w-[120px] h-[38px] mb-[10px] hover:bg-primary-600 capitalize font-bold text-[14px] text-center fixed bottom-0 left-1/2 -translate-x-1/2"
                         @click="toggleModal">
                         <div>close window</div>
                     </div>
                 </div>
-               
-                
 
             </div>
         </div>
@@ -87,7 +79,6 @@ export default {
     },
     computed: {
         pdfSrc() {
-            //   if (this.src.startsWith("data:image")) return this.src;
             return this.fetchService.getAsset(this.user.resumeURL);
         },
     },
@@ -101,20 +92,17 @@ export default {
     },
     methods: {
         async toggleModal() {
-            console.log('test')
             this.modalActive = !this.modalActive;
         },
         async handleProfileUpdate() {
 
             const formData = new FormData();
 
-            // console.log(Object.fromEntries(formData))
-
             if (this.resumeFile != null) formData.append('resumeFile', this.resumeFile);
 
-            // this.userFetchService.fetchJsonFile(`/${this.user.id}/resume`, "PUT", formData).then((response) => {
-            //     localStorage.setItem("user", JSON.stringify(response));
-            // })
+            this.userFetchService.fetchJsonFile(`/${this.user.id}/resume`, "PUT", formData).then((response) => {
+                localStorage.setItem("user", JSON.stringify(response));
+            })
 
             this.resumeFile = null;
         },
